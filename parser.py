3e44715f87aa1f546faea4ccf312d115c1352a5c
Emory Walsh <ewalsh00@stuy.edu>
2020-03-06 11:45:18 -0500

    Fixed comment spacing

--- parser.py
+++ parser.py
@@ -14,27 +14,29 @@ The file follows the following format:
 	          takes 8 arguments (x0, y0, x1, y1, rx0, ry0, rx1, ry1)
 	 bezier: add a bezier curve to the edge matrix -
 	         takes 8 arguments (x0, y0, x1, y1, x2, y2, x3, y3)
-         line: add a line to the edge matrix -
-               takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-         ident: set the transform matrix to the identity matrix -
-         scale: create a scale matrix,
-                then multiply the transform matrix by the scale matrix -
-                takes 3 arguments (sx, sy, sz)
-         move: create a translation matrix,
-               then multiply the transform matrix by the translation matrix -
-               takes 3 arguments (tx, ty, tz)
-         rotate: create a rotation matrix,
-                 then multiply the transform matrix by the rotation matrix -
-                 takes 2 arguments (axis, theta) axis should be x y or z
-         apply: apply the current transformation matrix to the edge matrix
-         display: clear the screen, then
-                  draw the lines of the edge matrix to the screen
-                  display the screen
-         save: clear the screen, then
-               draw the lines of the edge matrix to the screen
-               save the screen to a file -
-               takes 1 argument (file name)
-         quit: end parsing
+
+     line: add a line to the edge matrix -
+             takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
+     ident: set the transform matrix to the identity matrix -
+     scale: create a scale matrix,
+             then multiply the transform matrix by the scale matrix -
+             takes 3 arguments (sx, sy, sz)
+     move: create a translation matrix,
+             then multiply the transform matrix by the translation matrix -
+             takes 3 arguments (tx, ty, tz)
+     rotate: create a rotation matrix,
+             then multiply the transform matrix by the rotation matrix -
+             takes 2 arguments (axis, theta) axis should be x y or z
+      apply: apply the current transformation matrix to the edge matrix
+      display: clear the screen, then
+             draw the lines of the edge matrix to the screen
+             display the screen
+      save: clear the screen, then
+             draw the lines of the edge matrix to the screen
+             save the screen to a file -
+             takes 1 argument (file name)
+      quit: end parsing
+
 See the file script for an example of the file format
 """
 ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save' ]
